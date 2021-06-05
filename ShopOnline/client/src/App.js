674ad78ddcbd35d39f36f0a24e3fbf7dca88c782
Mohamed Amine Chatti <85394302+MedAmineChatti@users.noreply.github.com>
2021-06-05 12:44:14 +0100

    Add files via upload

---
+++ ShopOnline/client/src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import {BrowserRouter as Router} from 'react-router-dom'
+import {DataProvider} from './GlobalState'
+import Header from './components/headers/Header'
+import MainPages from './components/mainpages/Pages'
+import Footer from './components/footer/Footer'
+
+function App() {
+  return (
+    <DataProvider>
+      <Router>
+        <div className="App">
+          <Header />
+          <MainPages />
+          <br/><br/><br/><br/><br/><br/>
+          <Footer/>
+        </div>
+      </Router>
+    </DataProvider>
+  );
+}
+
+export default App;
---
+++ ShopOnline/client/src/components/footer/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Footer() {
+  return (
+          <footer>
+              <div>
+                  <p><center>PC SHOP © Copyright 2021. All Rights Reserved.</center></p>
+            </div>
+          </footer>
+  )
+}
+
+export default Footer
